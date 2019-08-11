f4fa24722193d76fab86c36cd16eafc081d9b315
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 15:02:22 -0600

    Add more tests - passing

--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
-import leaYear from "./index";
-
+import leapYear from "./index";
 it("should NOT be a leap year if divisible by 100, not 400", () => {
   const input = 1900;
   const expectedOutput = false;
